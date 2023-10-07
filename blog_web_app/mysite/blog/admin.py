a6950c3ece3615cf88e3551f3c8ceac6cfe89fc5
Yoseph Zemede <josephzed35@gmail.com>
2023-10-07 20:09:20 +0300

    Configured the administration site

--- blog_web_app/mysite/blog/admin.py
+++ blog_web_app/mysite/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    raw_id_fields = ['author',]
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
